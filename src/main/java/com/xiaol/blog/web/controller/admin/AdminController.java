626e5bc8bf453c52412b4bbfe27d2bb195bf11fb
Pickacat <ken1992149@163.com>
2017-02-13 23:03:37 +0800

    创建两个基本控制器

---
+++ src/main/java/com/xiaol/blog/web/controller/admin/AdminController.java
@@ -0,0 +1,18 @@
+package com.xiaol.blog.web.controller.admin;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @Description 博主控制器，用于发布文章等
+ * @date 创建时间：2017年2月12日 下午11:11:06
+ */
+@Controller
+@RequestMapping
+public class AdminController {
+
+	public String Login() {
+		return null;
+	}
+
+}
