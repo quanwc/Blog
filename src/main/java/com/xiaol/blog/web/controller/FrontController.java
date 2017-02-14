c77f2f3d9b37ac09ad674ef7a1149fe059a55119
Pickacat <ken1992149@163.com>
2017-02-14 20:57:31 +0800

    确定前端控制器部分方法

---
+++ src/main/java/com/xiaol/blog/web/controller/FrontController.java
@@ -0,0 +1,33 @@
+package com.xiaol.blog.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @Description 前端页面控制器，包括login、projects、archives、tags、about
+ * @date 创建时间：2017年2月13日 下午11:01:41
+ */
+@Controller
+@RequestMapping
+public class FrontController {
+
+	public String login() {
+		return null;
+	}
+
+	public String getProjects() {
+		return null;
+	}
+
+	public String getArchives() {
+		return null;
+	}
+
+	public String getTags() {
+		return null;
+	}
+
+	public String about() {
+		return null;
+	}
+}
